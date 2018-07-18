060b05872eaffb1b2b05ade986704d9080394151
Nick Thompson <Nick.thompson@rogers.com>
2018-07-17 20:45:31 -0400

    updated math.random + html tag

--- javascript/game.js
+++ javascript/game.js
@@ -5,30 +5,18 @@ window.onload = initAll;
 
 function initAll() {
     readyPlayerOne();
-    // let currentPlayer = 0;
     document.getElementById("square0").innerHTML = "X";
 }
 
 function readyPlayerOne() {
-    let currentPlayer = Math.floor(Math.random() + 1);
-    alert("currentPlayer = " + currentPlayer);
+    let currentPlayer = Math.round(Math.random());
     if (currentPlayer === 0) {
         alert("currentPlayer === 0");
-        document.getElementById("square1").innerHTML = currentPlayer;
+        document.getElementById("turnHolder").innerHTML = "X";
         
     }
     else {
         alert("currentPlayer != 0")
-        document.getElementById("square1").innerHTML = currentPlayer;
+        document.getElementById("turnHolder").innerHTML = "O";
     }
 }
-
-
-// $(document).ready(function() {
-//     window.alert("We're here!");
-//     document.getElementById(square0).innerHTML = 0;
-//     let currentPlayer = 0;
-//     readyPlayerOne(currentPlayer);
-//     document.getElementById(square0).innerHTML = 0;
-
-// })
